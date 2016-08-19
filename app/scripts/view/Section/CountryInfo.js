19852f87b7dc2d9fd4bbf18065430c088d293f32
Ekaterina Ivanova <kkateivanova@gmail.com>
2016-08-19 21:31:56 +0300

    Update CountryInfo.js

--- app/scripts/view/Section/CountryInfo.js
+++ app/scripts/view/Section/CountryInfo.js
@@ -3,6 +3,9 @@ export default class CountryInfo extends React.Component {
   constructor() {
     super();
   }
+  // REVIEW: предлагаю убрать отсюда id - 
+  // ты их нигде болье не используешь, а значит они излишни тут
+  // https://css-tricks.com/the-difference-between-id-and-class/#article-header-id-9
   render() {
     return (
       <div>
