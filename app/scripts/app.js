d59383c44544a5d73e2ae53b085fd9600acdc98d
Ekaterina Ivanova <kkateivanova@gmail.com>
2016-08-19 20:57:44 +0300

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Layout from './view/layout';
 import PageLayout from './page/layout';
+// Неиспользуемая переменная, в консоль warning'и не сыпятся?
 import Storage from './storage';
 
 export default class Application {
   constructor() {
+    // REVIEW: опять неиспользуемая переменная
     this.$document = $(document);
   }
   start() {
+    // REVIEW: из всего ReactDOM ты тут используешь только метод render,
+    // поэтому предлагаю оптимизировать и импортировать только метод render
+    // import {render} from 'react-dom';
+    // и тут вызывать уже его
+    // REVIEW: неочевидно названы компоненты - при первом взгляде на
+    // код рендера непонятно, что имеется в виду под Layout, а что - под PageLayout
     ReactDOM.render(
       <div className="application">
         <Layout />
