c18e2e881a585df70338f49ade4fd055df75403a
Artem <unnamed_9595@mail.ru>
2016-08-30 15:52:20 +0200

    create changing store in input

--- app/scripts/actions/actions.js
+++ app/scripts/actions/actions.js
@@ -1,82 +1,78 @@
-// косяк с модулем
-
-export default class Actions {
-  changeFullName(fullName) {
-    return {
-      type: "CHANGE_FULL_NAME",
-      payload: fullName,
-    };
+export const changeFullName = (fullName) => {
+  return {
+    type: "CHANGE_FULL_NAME",
+    payload: fullName,
   };
-  changeVacancy(vacancy) {
-    return {
-      type: "CHANGE_VACANCY",
-      payload: vacancy,
-    };
+};
+export const changeVacancy = (vacancy) => {
+  return {
+   type: "CHANGE_VACANCY",
+    payload: vacancy,
   };
-  cangeCash(cash) {
-    return {
-      type: "CHANGE_CASH",
-      payload: cash,
-    };
+};
+export const changeCash = (cash) => {
+  return {
+    type: "CHANGE_CASH",
+    payload: cash,
   };
-  changePhone(phone) {
-    return {
-      type: "CHANGE_PHONE",
-      payload: phone,
-    };
+};
+export const changePhone = (phone) => {
+  return {
+    type: "CHANGE_PHONE",
+    payload: phone,
   };
-  changeMail(mail) {
-    return {
-      type: "CHANGE_MAIL",
-      payload: mail,
-    };
+};
+export const changeMail = (mail) => {
+  return {
+    type: "CHANGE_MAIL",
+    payload: mail,
   };
-  changeNationality(nationality) {
-    return {
-      type: "CHANGE_NATIONALITY",
-      payload: nationality,
-    };
+};
+export const changeNationality = (nationality) => {
+  return {
+    type: "CHANGE_NATIONALITY",
+    payload: nationality,
   };
-  changeSex(sex) {
-    return {
-      type: "CHANGE_SEX",
-      payload: sex,
-    };
+};
+export const changeSex = (sex) => {
+  return {
+    type: "CHANGE_SEX",
+    payload: sex,
   };
-  changeAge(age) {
-    return {
-      type: "CHANGE_AGE",
-      payload: age,
-    };
+};
+export const changeAge = (age) => {
+  return {
+    type: "CHANGE_AGE",
+    payload: age,
   };
-  changeMarried(married) {
-    return {
-      type: "CHANGE_MARRIED",
-      payload: married,
-    };
+};
+export const changeMarried = (married) => {
+  return {
+    type: "CHANGE_MARRIED",
+    payload: married,
   };
-  changeEducation(education) {
-    return {
-      type: "CHANGE_EDUCATION",
-      payload: education,
-    };
+};
+export const changeEducation = (education) => {
+  return {
+    type: "CHANGE_EDUCATION",
+    payload: education,
   };
-  changeExperience(experience) {
-    return {
-      type: "CHANGE_EXPEREINCE",
-      payload: experience,
-    };
+};
+export const changeExperience = (experience)=> {
+  return {
+    type: "CHANGE_EXPEREINCE",
+    payload: experience,
   };
-  changeDate(date) {
-    return {
-      type: "CHANGE_DATE",
-      payload: date,
-    };
+};
+export const changeDate = (date) => {
+  return {
+    type: "CHANGE_DATE",
+    payload: date,
   };
-  changeValid(valid) {
-    return {
-      type: "CHANGE_VALID",
-      payload: valid,
-    };
+};
+export const changeValid = (valid) => {
+  return {
+    type: "CHANGE_VALID",
+    payload: valid,
   };
-}
+};
--- app/scripts/view/form.js
+++ app/scripts/view/form.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import {connect} from 'react-redux';
-// import* as actions from './../actions/actions';
-// чет какой то косяк с импортом
+import {
+  changeFullName,
+  changeVacancy,
+  changePhone,
+  changeMail,
+  changeSex,
+  changeAge,
+  changeNationality,
+  changeMarried,
+  changeDate,
+  changeEducation,
+  changeExperience,
+  changeValid,
+  changeCash
+} from './../actions/actions';
+
 @connect((store) => {
   return {
     user: store.user,
@@ -9,37 +23,64 @@ import {connect} from 'react-redux';
 })
 export default class Form extends React.Component {
   componentWillMount() {
-    // console.log(actions);
-    // this.props.dispatch(actions.changeFullName("LolLOOOOl"));
-    // не работает
-  }
 
+  }
+  updateVacancy(event) {
+    this.props.dispatch(changeVacancy(event.target.value));
+  }
+  updateCash(event) {
+    this.props.dispatch(changeCash(event.target.value));
+  }
+  updateName(event) {
+    this.props.dispatch(changeFullName(event.target.value));
+  }
+  updatePhone(event) {
+    this.props.dispatch(changePhone(event.target.value));
+  }
+  updateMail(event) {
+    this.props.dispatch(changeMail(event.target.value));
+  }
+  updateNationality(event) {
+    this.props.dispatch(changeNationality(event.target.value));
+  }
+  updateAge(event) {
+    this.props.dispatch(changeAge(event.target.value));
+  }
+  updateMarried(event) {
+    this.props.dispatch(changeMarried(event.target.value));
+  }
+  updateEducation(event) {
+    this.props.dispatch(changeEducation(event.target.value));
+  }
+  updateDate(event) {
+    this.props.dispatch(changeDate(event.target.value));
+  }
   render() {
     return (
       <section className="info">
         <span>Вакансия</span>
-        <input type="text" className="info--vacancy" />
+        <input type="text" className="info--vacancy" onChange={this.updateVacancy.bind(this)}/>
         <span>Желаемая зарплата</span>
-        <input type="text" className="info--cash" />
+        <input type="text" className="info--cash" onChange={this.updateCash.bind(this)}/>
         <span>ФИО</span>
-        <input type="text" className="info--fullName" />
+        <input type="text" className="info--fullName" onChange={this.updateName.bind(this)}/>
         <span>Контактный телефон</span>
-        <input type="text" className="info--phone" />
+        <input type="text" className="info--phone" onChange={this.updatePhone.bind(this)}/>
         <span>Mail</span>
-        <input type="text" className="info--mail" />
+        <input type="text" className="info--mail" onChange={this.updateMail.bind(this)}/>
         <span>Гражданство</span>
-        <input type="text" className="info--nationality" />
+        <input type="text" className="info--nationality" onChange={this.updateNationality.bind(this)}/>
         <span>Пол</span>
         <select name="sex" className="info--sex" >
           <option value="male">М</option>
           <option value="female">Ж</option>
         </select>
         <span>Возраст</span>
-        <input type="text" className="info--age" />
+        <input type="text" className="info--age" onChange={this.updateAge.bind(this)}/>
         <span>Семейное положение</span>
-        <input type="text" className="info--married" />
+        <input type="text" className="info--married" onChange={this.updateMarried.bind(this)}/>
         <span>Образование</span>
-        <input type="text" className="info--education" />
+        <input type="text" className="info--education" onChange={this.updateEducation.bind(this)}/>
         <span>Опыт работы</span>
         <select name="experience" className="info--experience" >
           <option value="none">Отсутствует</option>
@@ -49,7 +90,7 @@ export default class Form extends React.Component {
           <option value="moreFiveYear">Более 5 лет</option>
         </select>
         <span>Дата</span>
-        <input type="date" className="info--date" />
+        <input type="date" className="info--date" onChange={this.updateDate.bind(this)}/>
         <span>Разрешаю обработку личных данных</span>
         <input type="checkbox" className="valid" />
         <button className="submit" >Отправить </button>
