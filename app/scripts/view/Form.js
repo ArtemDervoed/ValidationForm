a2184c2fd9eb76b3e75fe5eced5c8603370ef035
Artem <unnamed_9595@mail.ru>
2016-09-20 16:29:16 +0300

    Add check before sending

--- app/scripts/view/Form.js
+++ app/scripts/view/Form.js
@@ -1,20 +1,26 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-alert */
 import FormRow from './FormRow';
+import {checked} from './FormRow';
 import React from 'react';
 import {connect} from 'react-redux';
 import * as API from './../API';
 
 export default class Form extends React.Component {
   onSubmit(event) {
-    console.log(this.props);
+    for (const i in checked) {
+      if (!checked[i]) {
+        alert('Не корректно заполнена форма');
+        return;
+      }
+    }
     API.addData(Math.random(), this.props.main);
   }
   render() {
     return (
       <form method="post" className="content--col">
         <FormRow name="Вакансия:" error="Обязательное поле" errorEmpty="Вы не заполнили поле"/>
-        <FormRow name="ФИО:" error="Введите полные ФИО" errorEmpty="Вы не заполнили поле"/>
+        <FormRow name="ФИО:" error="Возможно вы не полностью внесли свои ФИО или допустили ошибку" errorEmpty="Вы не заполнили поле"/>
         <FormRow name="Mail:" error="Поле должно быть вида userAdress@email.bb" errorEmpty="Вы не заполнили поле"/>
         <FormRow name="Контактный телефон:" error="Телефон должен быть вида 8-987-654-3210" errorEmpty="Вы не заполнили поле"/>
         <FormRow name="Опыт работы:" error="Обязательное поле" errorEmpty="Вы не заполнили поле"/>
--- app/scripts/view/FormRow.js
+++ app/scripts/view/FormRow.js
@@ -2,6 +2,16 @@ import React from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../actions/actions';
 
+export const checked = {
+  vacansy: false,
+  fullName: false,
+  phone: false,
+  mail: false,
+  education: false,
+  experience: false,
+  date: false,
+};
+
 export default class DataRow extends React.Component {
   inputBlurEventHandler(event) {
     const emptyErrorMessage = document.querySelectorAll('.form--row-error-empty');
@@ -9,30 +19,37 @@ export default class DataRow extends React.Component {
     switch (this.props.name) {
       case 'Вакансия:': {
         emptyErrorMessage[0].style.display = (event.target.value.length > 0) ? "none" : "block";
+        checked.vacansy = event.target.value.length > 0;
         errorMessage[0].style.display = "none";
       } break;
       case 'ФИО:': {
         emptyErrorMessage[1].style.display = (event.target.value.length > 0) ? "none" : "block";
+        checked.fullName = event.target.value.length > 0;
         errorMessage[1].style.display = "none";
       } break;
       case 'Контактный телефон:': {
         emptyErrorMessage[3].style.display = (event.target.value.length > 0) ? "none" : "block";
+        checked.phone = event.target.value.length > 0;
         errorMessage[3].style.display = "none";
       } break;
       case 'Mail:': {
         emptyErrorMessage[2].style.display = (event.target.value.length > 0) ? "none" : "block";
+        checked.mail = event.target.value.length > 0;
         errorMessage[2].style.display = "none";
       } break;
       case 'Опыт работы:': {
         emptyErrorMessage[4].style.display = (event.target.value.length > 0) ? "none" : "block";
+        checked.experience = event.target.value.length > 0;
         errorMessage[4].style.display = "none";
       } break;
       case 'Образование:': {
         emptyErrorMessage[5].style.display = (event.target.value.length > 0) ? "none" : "block";
+        checked.education = event.target.value.length > 0;
         errorMessage[5].style.display = "none";
       } break;
       case 'Дата заполнения:': {
         emptyErrorMessage[6].style.display = (event.target.value.length > 0) ? "none" : "block";
+        checked.date = event.target.value.length > 0;
         errorMessage[6].style.display = "none";
       } break;
       default: return undefined;
@@ -45,40 +62,47 @@ export default class DataRow extends React.Component {
       case 'Вакансия:': {
         this.props.dispatch(actions.changeVacancy(event.target.value));
         errorMessage[0].style.display = (event.target.value.length > 0) ? "none" : "block";
+        checked.vacansy = event.target.value.length > 0;
         emptyErrorMessage[0].style.display = "none";
       } break;
       case 'ФИО:': {
         this.props.dispatch(actions.changeFullName(event.target.value));
         const fullName = /^[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+$/;
         errorMessage[1].style.display = (fullName.test(event.target.value)) ? "none" : "block";
+        checked.fullName = fullName.test(event.target.value) > 0;
         emptyErrorMessage[1].style.display = "none";
       } break;
       case 'Контактный телефон:': {
         this.props.dispatch(actions.changePhone(event.target.value));
         const phone = /\d{1}-\d{3}-\d{3}-\d{4}/;
         errorMessage[3].style.display = (phone.test(event.target.value)) ? "none" : "block";
+        checked.phone = phone.test(event.target.value) > 0;
         emptyErrorMessage[3].style.display = "none";
       } break;
       case 'Mail:': {
         this.props.dispatch(actions.changeMail(event.target.value));
         const mail = /[0-9a-z_]+@[0-9a-z_^\.]+\.[a-z]{2,3}/;
         errorMessage[2].style.display = (mail.test(event.target.value)) ? "none" : "block";
+        checked.mail = mail.test(event.target.value) > 0;
         emptyErrorMessage[2].style.display = "none";
       } break;
       case 'Опыт работы:': {
         this.props.dispatch(actions.changeExperience(event.target.value));
         errorMessage[4].style.display = (event.target.value.length > 0) ? "none" : "block";
+        checked.experience = event.target.value.length > 0;
         emptyErrorMessage[4].style.display = "none";
       } break;
       case 'Образование:': {
         this.props.dispatch(actions.changeEducation(event.target.value));
         errorMessage[5].style.display = (event.target.value.length > 0) ? "none" : "block";
+        checked.education = event.target.value.length > 0;
         emptyErrorMessage[5].style.display = "none";
       } break;
       case 'Дата заполнения:': {
         this.props.dispatch(actions.changeDate(event.target.value));
         const date = /(0[1-9]|[12][0-9]|3[01])[- ..](0[1-9]|1[012])[- ..](19|20)\d\d/g;
         errorMessage[6].style.display = (date.test(event.target.value)) ? "none" : "block";
+        checked.date = date.test(event.target.value) > 0;
         emptyErrorMessage[6].style.display = "none";
       } break;
       default: return undefined;
